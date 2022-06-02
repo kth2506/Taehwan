90a48a2c77e0ce5670ee402f55d51a628fdf5705
kim taehwan <ksj4159@naver.com>
2022-06-02 12:51:27 +0900

    Vector ver 0.6

--- Vector/Vector/main.cpp
+++ Vector/Vector/main.cpp
@@ -1,40 +1,53 @@
-// ** Vector v0.4
+// ** Vector ver 0.6
 #include <iostream>
 
 using namespace std;
 
 int main(void)
 {
-	char str1[2][5] = {
-		{'A', 'B', 'C', 'D', 'E'},
-		{'F', 'G', 'H', 'I', 'J'}
-	};
-	char* str2[2] = {
-		{(char*)"ABCD"},
-		{(char*)"EFGH"}
 
-	};
 
-	printf_s("%s\n", str2[0]);
-	printf("%c", *str1[0] + 1);
-	printf("%c", str1[0][0]);
-	cout << (char)(*str2[0] + 4) << endl;
+
+
+
+
 	return 0;
-	// 1. 데이터 추가 (새로운 공간 확보)
-	
-	// 2. 데이터 삭제
-
-	// 3. 데이터를 삽입할수 있는 공간의 크기
-
-	// 4. 현재 삽입되어있는 원소의 개수
-	
-	// 5. container의 시작 지점.
-	
-	// 6. container의 종료 지점.
-
 
 }
+/*
+- 보다는 +(-) 가 더 효율적이고 (2의 보수)
+/ 보다는 *가 /2 보다는 >> 1 이 더 효율적이다.
+따라서 배열의 길이는 2^n 으로 해주는것이 효율적이다.
 
+배열은 0이 아닌값으로만 초기화가 가능하다
+배열은 상수값으로만 초기화가 가능하다
+
+아래와 같이 사용한다면 위 조건은 무시 가능
+
+	
+	int Size = 10;
+	int iter = 0;
+	int* Vector = new int[Size];
+
+	for (int i = 0; i < Size; ++i)
+		Vector[i] = i;
+
+	for (int i = 0; i < Size; ++i)
+		cout << Vector[i];
+	for (int i = 0; i < 17; ++i)
+		cout << Vector[iter++] << endl;
+*/
+// 1. 데이터 추가 (새로운 공간 확보)
+
+// 2. 데이터 삭제
+
+// 3. 데이터를 삽입할수 있는 공간의 크기
+
+// 4. 현재 삽입되어있는 원소의 개수
+
+// 5. container의 시작 지점.
+
+// 6. container의 종료 지점.
 
 
 /*
